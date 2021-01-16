94a46cb44a6160b3e53843b76c373b5e78975261
Anirudh Chamoli <chamolianirudh03@gmail.com>
2021-01-16 19:50:21 +0530

    Constraints

---
+++ chain.js
@@ -0,0 +1,23 @@
+class Chain {
+    constructor (bodyA , bodyB)
+    {
+        var options={
+            bodyA : bodyA ,
+            bodyB : bodyB ,
+            length : 50,
+            stiffness : 0.3
+        }
+
+        this.chain = Constraints.create(options)
+        World.add(world , this.chain)
+    }
+
+    display ()
+    {
+        var posA = this.chain.bodyA.position
+        var posB = this.chain.bodyB.position
+
+        strokeWeight (5)
+        line ( posA.x , posA.y , posB.x , posB.y )
+    }
+}
